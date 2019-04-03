243acac08d0484fd7d80d35cbcc6cac63404e080
Jared <jared@updatepromise.com>
2019-04-03 14:51:35 -0700

    netcat tool coming to fruition one step at a time

--- netcat.py
+++ netcat.py
@@ -13,6 +13,7 @@ target = ""
 upload_destination = ""
 port = 0
 
+
 def usage():
     print "Netcat Net Tool"
     print
@@ -28,3 +29,49 @@ def usage():
     print
     print "Examples: "
     print "netcat.py -t 192.168.0.1 -p"
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu", [
+        "help", "listen", "execute", "target", "port", "command", "upload"])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    for o,a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-e", "--execute"):
+            execute = a
+        elif o in ("-c", "--command"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"
+
+    if not listen and len(target) and port > 0:
+        buffer = sys.stdin.read()
+        client_sender(buffer)
+
+    if listen:
+        server_loop()
+
+    main()
+
