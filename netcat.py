9b9b475bc70c339798df1dab8f8e25f49f519ace
Jared <jared@updatepromise.com>
2019-04-04 17:33:45 -0700

    net cat proof on foncept completed

--- netcat.py
+++ netcat.py
@@ -30,6 +30,108 @@ def usage():
     print "Examples: "
     print "netcat.py -t 192.168.0.1 -p"
 
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        client.connect((target, port))
+
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+
+                if recv_len < 4096:
+                    break
+
+            print response,
+
+            buffer = raw_input("")
+            buffer += "\n"
+
+            client.send(buffer)
+    except:
+
+        print "[*] Exception! Exiting."
+        client.close()
+
+def server_loop():
+    global target
+
+    if not len(target):
+        target = "0.0.0.0"
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((target, port))
+
+    server.listen(5)
+
+    while True:
+        client_socket, addr = server.accept()
+
+        client_thread = threading.Thread(
+            target = client_handler, args = (client_socket,))
+        client_thread.start()
+
+
+def run_command(command):
+    command = command.rstrip()
+
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = "Failed to execute command. \r\n"
+
+    return output
+
+def client_handler(client_socket):
+    global upload
+    global execute
+    global command
+
+    if len(upload_destination):
+        file_buffer = ""
+
+        while True:
+            data = client_socket.recv(1024)
+
+            if not data:
+                break
+            else:
+                file_buffer += data
+
+            try:
+                file_descriptor = open(upload_destination, "wb")
+                file_descriptor.write(file_buffer)
+                file_descriptor.close()
+
+                client_socket.send("Successfully saved file to %s\r\n" % upload_destination)
+            except:
+                client_socket.send("Failed to save file to %s\r\n" % upload_destination)
+
+    if len(execute):
+        output = run_command(execute)
+        client_socket.send(output)
+
+    if command:
+        while True:
+            client_socket.send("<BHP:#> ")
+
+            cmd_buffer = ""
+            while "\n" not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+
+            response = run_command(cmd_buffer)
+            client_socket.send(response)
+
+
 def main():
     global listen
     global port
@@ -73,5 +175,5 @@ def main():
     if listen:
         server_loop()
 
-    main()
+main()
 
