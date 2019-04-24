10ea983a9b5e822b0145dd3c70694408b313a09d
Jared <jared@updatepromise.com>
2019-04-23 21:08:40 -0700

    the unpack function in __new__ is not working as intended

--- sniffer.py
+++ sniffer.py
@@ -1,8 +1,42 @@
 import socket
 import os
+import struct
+from ctypes import *
+
 
 # THIS SHOULD BE THE IP OF YOUR COMPUTER
 host = "10.1.20.59"
+
+class IP(Structure):
+    _fields = [
+        ("ihl", c_ubyte, 4),
+        ("version", c_ubyte, 4),
+        ("tos", c_ubyte),
+        ("len", c_ushort),
+        ("id", c_ushort),
+        ("offset", c_ushort),
+        ("ttl", c_ubyte),
+        ("protocol_num", c_ubyte),
+        ("sum", c_ushort),
+        ("src", c_ulong),
+        ("dst", c_ulong)
+    ]
+
+    def __new__(self, socket_buffer=None):
+        print socket_buffer[0:3]
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
+
+        self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
+
 
 if os.name == "nt":
     socket_protocol = socket.IPPROTO_IP
@@ -22,3 +56,17 @@ print sniffer.recvfrom(65565)
 
 if os.name == "nt":
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+try:
+    while True:
+        raw_buffer = sniffer.recvfrom(65565)[0]
+
+        ip_header = IP(raw_buffer[0:20])
+
+        print "Protocol: %s %s -> %s" % (ip_headed.protocol,
+        ip_header.src_address, ip_headed.dst_address)
+
+except KeyboardInterrupt:
+    if os.name == "nt":
+        sniffer.iotcl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
