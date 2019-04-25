19d9f0d084ff6e0429202486ec798e6b5d381445
Jared <jared@updatepromise.com>
2019-04-25 13:43:51 -0700

    icmp basic decode completed, moving to test more extensive sniffer

--- sniffer.py
+++ sniffer.py
@@ -8,7 +8,7 @@ from ctypes import *
 host = "10.1.20.59"
 
 class IP(Structure):
-    _fields = [
+    _fields_ = [
         ("ihl", c_ubyte, 4),
         ("version", c_ubyte, 4),
         ("tos", c_ubyte),
@@ -18,17 +18,18 @@ class IP(Structure):
         ("ttl", c_ubyte),
         ("protocol_num", c_ubyte),
         ("sum", c_ushort),
-        ("src", c_ulong),
-        ("dst", c_ulong)
+        # x86 Ubuntu
+        # ("src", c_ulong),
+        # ("dst", c_ulong)
+        ("src", c_uint32),
+        ("dst", c_uint32)
     ]
 
     def __new__(self, socket_buffer=None):
-        print socket_buffer[0:3]
         return self.from_buffer_copy(socket_buffer)
 
     def __init__(self, socket_buffer=None):
         self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
-
         self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
         self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
 
@@ -61,10 +62,11 @@ try:
     while True:
         raw_buffer = sniffer.recvfrom(65565)[0]
 
+        # ip_header = IP(raw_buffer)
         ip_header = IP(raw_buffer[0:20])
 
-        print "Protocol: %s %s -> %s" % (ip_headed.protocol,
-        ip_header.src_address, ip_headed.dst_address)
+        print "Protocol: %s %s -> %s" % (ip_header.protocol,
+        ip_header.src_address, ip_header.dst_address)
 
 except KeyboardInterrupt:
     if os.name == "nt":
